e112a016944ba8f740b39638b19719cf4be7539e
RyGuyFlinn <ryguyflinn@gmail.com>
2024-07-19 22:37:30 -0700

    blah

--- app.py
+++ app.py
@@ -1,58 +1,19 @@
-# This function adds two numbers
-def add(x, y):
-    return x + y
+import cv2
 
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
 
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
 
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
 
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-
-# This function divides two numbers
-def divide(x, y):
-    return x / y
-
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
-while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
-
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-            break
-    else:
-        print("Invalid Input")
+vc.release()
+cv2.destroyWindow("preview")
